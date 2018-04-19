12cf9766a885129e4336623149ff8c9072c20e87
Robin Le Bihan <le_bih_r@epita.fr>
2018-04-19 21:57:30 +0200

    db: start #1 with Association model
    Added field to the association model:
    * name
    * website
    * email
    * logo
    * president

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Association(models.Model):
+    name = models.CharField(max_length=150)
+    website = models.CharField(max_length=200)
+    email = models.EmailField()
+    logo = models.ImageField()
+    president = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True,
+                                  null=True)
