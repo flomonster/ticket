1b29a579cd651b13c219b18b78afce52c6790158
Robin Le Bihan <le_bih_r@epita.fr>
2018-04-20 12:30:30 +0200

    db: fix #1 issue (creation of models)

--- core/models.py
+++ core/models.py
@@ -13,6 +13,9 @@ class EventStatus:
 
 
 class MemberType:
+    """
+    Defines an enum to tag a membership relation
+    """
     OFFICE = 1
     SIMPLE = 2
     PRESIDENT = 3
@@ -28,13 +31,13 @@ class Association(models.Model):
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    start = models.DateTimeField()
-    end = models.DecimalField()
+    start = models.DateTimeField() # Start of the event
+    end = models.DecimalField() # End of the event
     place = models.CharField(max_length=200)
     cover = models.ImageField(blank=True)
     orga = models.ForeignKey(Association, on_delete=models.DO_NOTHING)
-    closing = models.DateTimeField()
-    int_capacity = models.IntegerField()
+    closing = models.DateTimeField() # Date of subscription closing
+    int_capacity = models.IntegerField() # Available tickets for members
     ext_capacity = models.IntegerField()
     int_price = models.IntegerField()
     ext_price = models.IntegerField()
@@ -44,17 +47,26 @@ class Event(models.Model):
 
 
 class Staff(models.Model):
+    """
+    An user is a staff member of an event
+    """
     event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
     member = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
 
 class Membership(models.Model):
+    """
+    An user is a member of an association
+    """
     asso = models.ForeignKey(Association, on_delete=models.DO_NOTHING)
     member = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     type = models.SmallIntegerField()
 
 
 class Participant(models.Model):
+    """
+    An user has subscribed for an event
+    """
     paid = models.IntegerField()
     event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
