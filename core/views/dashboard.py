6a164cd6ab1cf91769676a7e71a0a7682ada6244
Robin Le Bihan <le_bih_r@epita.fr>
2018-04-20 18:46:59 +0200

    dash: add check if associaiton exists

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 
+from core.models import Association
+
+def asso_not_found(name):
+    return HttpResponse('No such association ' + name)
 
 def view(request, name):
-    return HttpResponse('Dashboard')
+    asso = Association.objects.filter(name=name)
+
+    if not asso:
+        return asso_not_found(name)
+
+    return HttpResponse('Dashboard ' + name)
