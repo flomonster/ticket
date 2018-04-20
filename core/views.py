1bc99bf04c3c52859674efe5a2ee2e90a6bf24f6
Claire Hayard <claire.hayard@epita.fr>
2018-04-20 12:52:22 +0200

    association: add association view to core app

--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from core.association.associationCreate import createAssociationView
+
+
 def index(request):
     return HttpResponse("Acceuil")
+
+def createAssociation(request):
+   return createAssociationView(request)
