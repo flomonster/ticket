7f5abc0033b4b0513e4ac9b555515457ad04da34
Robin Le Bihan <le_bih_r@epita.fr>
2018-04-21 11:14:40 +0200

    dash: add function to get related events

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 
-from core.models import Association
+from core.models import Association, Event
+
 
 def asso_not_found(name):
     return HttpResponse('No such association ' + name)
+
 
 def view(request, name):
     asso = Association.objects.filter(name=name)
@@ -11,4 +13,12 @@ def view(request, name):
     if not asso:
         return asso_not_found(name)
 
+    asso = asso[0]
+
+    print(related_events(asso))
     return HttpResponse('Dashboard ' + name)
+
+
+def related_events(asso):
+    e = Event.objects.select_related('orga').filter(orga__exact=asso)
+    return e
