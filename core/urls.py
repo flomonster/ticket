2364ed26535c8d0e77509ef0f8e5d40ede8eeaae
Florian Amsallem <amsall_f@epita.fr>
2018-04-22 15:46:49 +0200

    login: #2 add urls

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
 from core.views import index, dashboard
+from django.contrib.auth import views as auth_views
 
 app_name = 'core'
 
 urlpatterns = [
     path('', index.view, name='index'),
+
+    # Login
+    path('login/', auth_views.login, name='login'),
+    path('logout/', auth_views.logout, name='logout'),
+
+    # Associations
     path('association/<str:name>/', dashboard.view),
     path('association/<str:name>/remove/<str:member>/', dashboard.delete_office_view)
+
 ]
--- ticket/urls.py
+++ ticket/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('billeterie/', include('core.urls'))
+    path('billeterie/', include('core.urls')),
+    path('account/', include(('social_django.urls', 'social'))),
+    path('account/', include(('django.contrib.auth.urls', 'auth'))),
 ]
