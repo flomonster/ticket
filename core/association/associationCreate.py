a2aca93c0b65aab8c6d11b30e063be2a01058192
Claire Hayard <claire.hayard@epita.fr>
2018-04-23 19:03:43 +0200

    association: #3 rename view association

--- core/association/associationCreate.py
+++ core/association/associationCreate.py
@@ -1,19 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-from .form import AssociationForm
+from .form import association_form
 
-def createAssociationView(request):
+
+def create_association_view(request):
     if request.method == 'POST':
-        form = AssociationForm(request.POST)
+        form = association_form(request.POST)
         if form.is_valid():
-            association = Association();
+            association = Association()
             association.name = form.cleaned_data['name']
             association.website = form.cleaned_data['website']
             association.email = form.cleaned_data['email']
             association.logo = form.cleaned_data['logo']
             association.save()
-            #return HttpResponseRedirect('/thanks/')
+            # return HttpResponseRedirect('/thanks/')
     else:
-        form = AssociationForm()
+        form = association_form()
 
-    return render(request, 'association.html', {'form' : form})
+    return render(request, 'association.html', {'form': form})
--- core/association/form.py
+++ core/association/form.py
@@ -1,7 +1,8 @@
 from django import forms
 
-class AssociationForm(forms.Form):
+
+class association_form(forms.Form):
     name = forms.CharField(label="Nom de l'association", max_length=150)
     website = forms.CharField(label="Site web", max_length=200)
-    email = forms.EmailField() #FIXME
+    email = forms.EmailField()  # FIXME
     logo = forms.ImageField()
