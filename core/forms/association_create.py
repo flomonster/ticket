1a1d5aa1d09e3aa61998e51e6cc1343a78e4ef13
Claire Hayard <claire.hayard@epita.fr>
2018-04-24 13:47:06 +0200

    forms: add required field to ImageField

--- core/forms/association_create.py
+++ core/forms/association_create.py
@@ -5,4 +5,4 @@ class association_form(forms.Form):
     name = forms.CharField(label="Nom de l'association", max_length=150)
     website = forms.CharField(label="Site web", max_length=200)
     email = forms.EmailField()  # FIXME
-    #logo = forms.ImageField()
+    logo = forms.ImageField(required=False)
