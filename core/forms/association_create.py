c0242f3462a00bb176664c39fd05a5a698f6b46e
Claire Hayard <claire.hayard@epita.fr>
2018-04-25 14:08:02 +0200

    form: add constructor for form class

--- core/forms/association_create.py
+++ core/forms/association_create.py
@@ -3,6 +3,11 @@ from django import forms
 
 class association_form(forms.Form):
     name = forms.CharField(label="Nom de l'association", max_length=150)
-    website = forms.CharField(label="Site web", max_length=200)
-    email = forms.EmailField()  # FIXME
-    logo = forms.ImageField(required=False)
+    website = forms.CharField(label="Site web", max_length=200, required=False)
+    email = forms.EmailField()
+    logo = forms.ImageField()
+
+    def __init__(self, *args, **kwargs):
+        super(association_form, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
