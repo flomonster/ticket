e225091844cc438bd3f29ba5007e6d856366c4bd
Robin Le Bihan <le_bih_r@epita.fr>
2018-04-26 14:29:48 +0200

    association: fix url of creation

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', index.view, name='index'),
 
     # Associations
+    path('association/create/', association_create.view, name='association_create'),
     path('association/<str:name>/', dashboard.Dashboard.view, name='association'),
     path('association/<str:name>/remove/office/<str:member>/',
         dashboard.Dashboard.delete_office_view, name='association_remove'),
