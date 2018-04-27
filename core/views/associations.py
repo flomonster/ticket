c8747a95b993e4244312f67e26404f5a3b338076
Robin Le Bihan <le_bih_r@epita.fr>
2018-04-27 16:58:54 +0200

    login: add login_required decorator

--- core/views/associations.py
+++ core/views/associations.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from core.models import Association
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def view(request):
     return render(request, 'associations.html', {'associations': Association.objects.all()})
