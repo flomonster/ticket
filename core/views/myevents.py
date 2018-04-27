6f2bc9d419c00fa79de8df127fa906169028582e
Robin Le Bihan <le_bih_r@epita.fr>
2018-04-27 20:35:41 +0200

    events: add basic layout for displaying events

--- core/views/myevents.py
+++ core/views/myevents.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 
+from core.models import Event, EventStatus
+
 class MyEvents:
     @staticmethod
     def view(request):
-        return render(request, 'my_events.html', {})
+        events = MyEvents.get_events()
+
+        # Template variables
+        variables = {}
+        variables['events'] = events
+        variables['waiting'] = str(EventStatus.WAITING._value_)
+        variables['validated'] = str(EventStatus.VALIDATED._value_)
+        variables['pending'] = str(EventStatus.PENDING._value_)
+
+        return render(request, 'my_events.html', variables)
+
+    @staticmethod
+    def get_events():
+        """
+        @brief get the set of events to be displayed
+        @return query set of Event
+        """
+        # TODO: refined events query
+        return Event.objects.all()
