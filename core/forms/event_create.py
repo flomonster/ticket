b0b3ae83bae950862267186088d28386ae71d341
Bertrand Ha <bertrand.ha@epita.fr>
2018-04-27 22:10:38 +0200

    event: adding form
    Some fields are still left empty atm

---
+++ core/forms/event_create.py
@@ -0,0 +1,28 @@
+from django import forms
+
+
+class event_form(forms.Form):
+    title = forms.CharField(label="Nom de l'évènement", max_length=200)
+    description = forms.CharField(label="Description", max_length=500, required=False)
+    start = forms.DateTimeField(label="Date de début")
+    end = forms.DateTimeField(label="Date de fin")
+    place = forms.CharField(label="Lieu de l'évènement", max_length=200)
+    cover = forms.ImageField()
+    orga =
+    closing = forms.DateTimeField(label="Date butoire d'inscription")
+    int_capacity = forms.IntegerField(label="Capacité maximale d'internes")
+    ext_capacity = forms.IntegerField(label="Capacité maximale d'externes")
+    int_price = forms.IntegerField(label="Prix internes")
+    ext_price = forms.IntegerField(label="Prix externes")
+    display = forms.BooleanField(label="")
+    status =
+    token =
+
+    website = forms.CharField(label="Site web", max_length=200, required=False)
+    email = forms.EmailField()
+    logo = forms.ImageField()
+
+    def __init__(self, *args, **kwargs):
+        super(association_form, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
