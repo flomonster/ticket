8bbaf73e60939aa9df8404539c0d17d861af7e22
Robin Le Bihan <le_bih_r@epita.fr>
2018-05-24 15:46:03 +0200

    assos: add all associations view for respo

--- core/views/associations.py
+++ core/views/associations.py
@@ -1,14 +1,28 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from core.models import Association
+from core.models import Association, Membership
 from django.urls import reverse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 @login_required
 def view(request):
-    return render(request, 'associations.html', {'associations': Association.objects.all()})
+    if request.user.has_perm('core.respo'):
+        assos = Association.objects.all()
+    else:
+        assos = get_associations(request.user)
 
-@login_required
+    variables = {}
+    variables['associations'] = assos
+    variables['respo'] = request.user.has_perm('core.respo')
+    return render(request, 'associations.html', variables)
+
+@permission_required('core.respo')
 def remove(request, name):
     Association.objects.get(name=name).delete()
     return redirect("core:associations")
+
+def get_associations(user):
+    assos = Membership.objects.filter(member__exact=user)\
+                      .select_related('asso')
+    assos = [p['asso'] for p in list(assos.values('asso').all())]
+    return Association.objects.filter(id__in=assos)
