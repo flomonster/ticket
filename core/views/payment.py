b4270062662053bbb1717ac283eeb5e04021e99a
Claire Hayard <claire.hayard@epita.fr>
2018-05-25 18:04:37 +0200

    payment: add paypal button

---
+++ core/views/payment.py
@@ -0,0 +1,8 @@
+from core.models import Event
+from django.shortcuts import render, get_object_or_404
+
+
+def view(request, id):
+    event = get_object_or_404(Event, pk=id)
+    context = {'price': event.int_price}
+    return render(request, 'payment.html', context)
