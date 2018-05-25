91f43aeeacb0234359e15aabc5a31388fdf5b1f0
Claire Hayard <claire.hayard@epita.fr>
2018-05-25 19:32:57 +0200

    payment: link payment page to register form

--- core/views/payment.py
+++ core/views/payment.py
@@ -1,7 +1,8 @@
 from core.models import Event
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def view(request, id):
     event = get_object_or_404(Event, pk=id)
     context = {'event': event}
