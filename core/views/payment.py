ec263e2098bab02e2f3f8ec4fea4de5ae8ab1703
Claire Hayard <claire.hayard@epita.fr>
2018-06-05 15:47:48 +0200

    views: added participant to payment view

--- core/views/payment.py
+++ core/views/payment.py
@@ -1,9 +1,10 @@
-from core.models import Event
+from core.models import Event, Participant
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def view(request, id):
     event = get_object_or_404(Event, pk=id)
-    context = {'event': event}
+    participant = Participant.objects.get(event=event, user=request.user)
+    context = {'event': event, 'participant': participant}
     return render(request, 'payment.html', context)
