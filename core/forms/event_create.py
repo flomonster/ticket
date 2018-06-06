58139dc284f56b761f6af5a692c063164f0a0557
Robin Le Bihan <le_bih_r@epita.fr>
2018-06-06 16:36:19 +0200

    event: fix form and css

--- core/forms/event_create.py
+++ core/forms/event_create.py
@@ -1,11 +1,12 @@
 from django import forms
+from django.forms import widgets
 
 from core.models import Association, EventStatus
 
 class event_form(forms.Form):
     title = forms.CharField(label="Nom de l'évènement", max_length=200)
     description = forms.CharField(label="Description", max_length=500, required=False)
-    start = forms.DateTimeField(label="Date de début")
+    start = forms.DateTimeField(label="Date de début", widget=widgets.DateTimeInput())
     end = forms.DateTimeField(label="Date de fin")
     place = forms.CharField(label="Lieu de l'évènement", max_length=200)
     cover = forms.ImageField()
@@ -17,13 +18,8 @@ class event_form(forms.Form):
     ext_price = forms.IntegerField(label="Prix externes")
     display = forms.BooleanField(label="Montrer les effectifs")
     status = EventStatus.WAITING
-    token = 42 #FIXME: change value later
-
-    website = forms.CharField(label="Site web", max_length=200, required=False)
-    email = forms.EmailField()
-    logo = forms.ImageField()
 
     def __init__(self, *args, **kwargs):
-        super(association_form, self).__init__(*args, **kwargs)
+        super(event_form, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
