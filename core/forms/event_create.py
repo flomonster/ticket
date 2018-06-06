f71a35101f7cc473c4fbf4c702baad7a8db8cf3e
Robin Le Bihan <le_bih_r@epita.fr>
2018-06-06 19:29:22 +0200

    event: fix form for event creation + token

--- core/forms/event_create.py
+++ core/forms/event_create.py
@@ -4,20 +4,58 @@ from django.forms import widgets
 from core.models import Association, EventStatus
 
 class event_form(forms.Form):
-    title = forms.CharField(label="Nom de l'évènement", max_length=200)
-    description = forms.CharField(label="Description", max_length=500, required=False)
-    start = forms.DateTimeField(label="Date de début", widget=widgets.DateTimeInput())
-    end = forms.DateTimeField(label="Date de fin")
-    place = forms.CharField(label="Lieu de l'évènement", max_length=200)
-    cover = forms.ImageField()
-    orga = forms.ModelChoiceField(queryset=Association.objects.all())
-    closing = forms.DateTimeField(label="Date butoire d'inscription")
-    int_capacity = forms.IntegerField(label="Capacité maximale d'internes")
-    ext_capacity = forms.IntegerField(label="Capacité maximale d'externes")
-    int_price = forms.IntegerField(label="Prix internes")
-    ext_price = forms.IntegerField(label="Prix externes")
-    display = forms.BooleanField(label="Montrer les effectifs")
-    status = EventStatus.WAITING
+    title = forms.CharField(
+            label="Nom de l'évènement", max_length=200,
+            widget=forms.TextInput(attrs={'placeholder': 'Mon évènement'})
+    )
+    description = forms.CharField(
+            label="Description",
+            max_length=500,
+            required=False,
+            widget=widgets.Textarea(attrs={'placeholder': 'Description de l\'évènement...'})
+    )
+    start = forms.DateTimeField(
+            label="Date de début",
+            widget=widgets.DateInput(attrs={'type':'datetime-local'}),
+            input_formats=['%Y-%m-%dT%H:%M']
+    )
+    end = forms.DateTimeField(
+            label="Date de fin",
+            widget=widgets.DateInput(attrs={'type':'datetime-local'}),
+            input_formats=['%Y-%m-%dT%H:%M']
+    )
+    place = forms.CharField(
+            label="Lieu de l'évènement", max_length=200,
+            widget=forms.TextInput(attrs={'placeholder': 'Kremlin-Bicêtre'})
+    )
+    cover = forms.ImageField(
+            label='Photo de couverture'
+    )
+    orga = forms.ModelChoiceField(
+            label='Association organisatrice',
+            queryset=Association.objects.all()
+    )
+    closing = forms.DateTimeField(
+            label="Date butoire d'inscription",
+            widget=widgets.DateInput(attrs={'type':'datetime-local'}),
+            input_formats=['%Y-%m-%dT%H:%M']
+    )
+    int_capacity = forms.IntegerField(
+            label="Capacité maximale d'internes", initial=0
+    )
+    ext_capacity = forms.IntegerField(
+            label="Capacité maximale d'externes", initial=0
+    )
+    int_price = forms.IntegerField(
+            label="Prix internes", initial=0
+    )
+    ext_price = forms.IntegerField(
+            label="Prix externes", initial=0
+    )
+    display = forms.BooleanField(
+            label="Montrer les effectifs",
+            widget=widgets.CheckboxInput(attrs={'type': 'checkbox'})
+    )
 
     def __init__(self, *args, **kwargs):
         super(event_form, self).__init__(*args, **kwargs)
