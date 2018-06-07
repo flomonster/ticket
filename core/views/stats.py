e965748f3e4eb24053919c60660b799c63b4e385
Robin Le Bihan <le_bih_r@epita.fr>
2018-06-07 23:04:35 +0200

    stats: add selection of date method

--- core/views/stats.py
+++ core/views/stats.py
@@ -1,9 +1,18 @@
 from django import forms
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+import pytz
 
 from core.models import Association, Event, Membership, MemberRole, EventStatus, Participant, Staff
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+
+class Constants:
+    methods = {'all': 0, 'trimester': 3, 'month': 1, 'semester': 6}
+    begin = dt.datetime(2018, 1, 1)
+    end = None
 
 class Stat:
     def __init__(self, **kwargs):
@@ -31,8 +40,17 @@ class RegisterStat(Stat):
     def __dir__(self):
         return ['ext_reg', 'ext', 'int', 'int_reg', 'staffs']
 
+def enddate(method, n):
+    date = Constants.begin + relativedelta(months=(n * Constants.methods[method]))
+    pytz.timezone(timezone.get_default_timezone_name()).localize(date)
+    return date
+
 def assoevents(asso):
     events = Event.objects.filter(orga__exact=asso)
+
+    if Constants.end is not None:
+        events = events.filter(start__gt=Constants.begin, end__lt=Constants.end)
+
     stat = EventStat(name=asso.name)
     stat.rejected = events.filter(status__exact=EventStatus.REJECTED._value_).count()
     stat.finished = events.filter(status__exact=EventStatus.FINISHED._value_).count()
@@ -64,14 +82,35 @@ def eventregister(event):
 
 def registerstats():
     events = Event.objects.all().order_by('start')
+    if Constants.end is not None:
+        events = events.filter(start__gt=Constants.begin, end__lt=Constants.end)
+
     return [eventregister(event) for event in events]
 
+def allintervals(method):
+    begin = Constants.begin
+    i = 1
+    while enddate(method, i) < dt.datetime.now():
+        i += 1
+
+    return i
+
+pytz.timezone(timezone.get_default_timezone_name()).localize(Constants.begin)
+
 @login_required
-def view(request):
+def view(request, method='all', no=1):
+    if method != 'all':
+        if not method in Constants.methods.keys() or no > allintervals(method) or no <= 0:
+            return redirect(reverse('core:my_events'))
+
+    if method != 'all':
+        Constants.end = enddate(method, no)
+
     event_stats = eventstats()
 
     variables = {}
     variables['event_stats'] = event_stats
     variables['register_stats'] = registerstats()
+    variables['method'] = method
 
     return render(request, 'stats.html', variables)
