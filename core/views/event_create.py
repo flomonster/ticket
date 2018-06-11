6623ad0d19f9ac70c28bd85231600a33cb91d21b
Robin Le Bihan <le_bih_r@epita.fr>
2018-06-11 15:38:07 +0200

    event: fix date and time input

--- core/views/event_create.py
+++ core/views/event_create.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from core.models import Event, EventStatus
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 from core.forms.event_create import event_form
 from core.models import Association
@@ -16,7 +17,7 @@ def view(request, asso_id):
     asso = Association.objects.get(pk=asso_id)
     if request.method == 'POST':
         form = event_form(request.POST, request.FILES)
-        print(request.POST)
+        print(form.errors)
         if form.is_valid():
             event = Event.objects.all().filter(title=form.cleaned_data['title'])
             if event.count() != 0:
@@ -25,12 +26,23 @@ def view(request, asso_id):
             evt = Event()
             evt.title = form.cleaned_data['title']
             evt.description = form.cleaned_data['description']
-            evt.start = form.cleaned_data['start']
-            evt.end = form.cleaned_data['end']
+
+            start_date = form.cleaned_data['start_date']
+            start_time = form.cleaned_data['start_time']
+            evt.start = datetime.strptime(start_date + ' ' + start_time, '%Y-%m-%d %H:%M')
+
+            end_date = form.cleaned_data['end_date']
+            end_time = form.cleaned_data['end_time']
+            evt.end = datetime.strptime(end_date + ' ' + end_time, '%Y-%m-%d %H:%M')
+
             evt.place = form.cleaned_data['place']
             evt.cover = form.cleaned_data['cover']
             evt.orga = asso
-            evt.closing = form.cleaned_data['closing']
+
+            closing_date = form.cleaned_data['closing_date']
+            closing_time = form.cleaned_data['closing_time']
+            evt.closing = datetime.strptime(closing_date + ' ' + closing_time, '%Y-%m-%d %H:%M')
+
             evt.int_capacity = form.cleaned_data['int_capacity']
             evt.ext_capacity = form.cleaned_data['ext_capacity']
             evt.int_price = form.cleaned_data['int_price']
