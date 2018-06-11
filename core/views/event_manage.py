8985ed8c96b659008cf848f801aecd6b3bfc11ba
Robin Le Bihan <le_bih_r@epita.fr>
2018-06-11 19:49:26 +0200

    event: add buttons to add and remove staff

--- core/views/event_manage.py
+++ core/views/event_manage.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 from core.views.event import manager_check
-from core.models import Event
+from core.models import Event, Staff
 
 def view(request, id):
     event = get_object_or_404(Event, id=id)
     if not manager_check(event, request.user):
         return redirect(reverse('core:event', args=[event.id]))
 
-    return render(request, 'event_manage.html', {'event': event})
+    staff = get_staff(event)
+
+    variables = {}
+    variables['event'] = event
+    variables['staff'] = staff
+
+    return render(request, 'event_manage.html', variables)
+
+def get_staff(event):
+    return Staff.objects.filter(event=event)
