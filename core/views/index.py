9b8d5b73cd9416bd5998fb1ca801fa9d7810e970
Robin Le Bihan <le_bih_r@epita.fr>
2018-06-13 16:42:42 +0200

    index: add basic calendar display

--- core/views/index.py
+++ core/views/index.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.contrib import auth
+from django.utils import timezone
+import datetime
+import pytz
 
 from core.models import Event, EventStatus
 from ticket.settings import STATIC_URL
@@ -10,11 +13,47 @@ def get_events():
                         .filter(status__exact=EventStatus.VALIDATED._value_)\
                         .order_by('-premium', '-int_price', '-ext_price')
 
+class Entry:
+    def __init__(self):
+        pass
+
+def initcalendar():
+    L = []
+    actual = timezone.now()
+    allevents = Event.objects.filter(status__exact=EventStatus.VALIDATED._value_)
+    d = 0
+
+    for i in range(10):
+        tmp = []
+        for j in range(3):
+            e = Entry()
+            e.date = (actual + (d * datetime.timedelta(days=1))).date()
+
+            start = actual + (d * datetime.timedelta(days=1))
+            end = actual + ((d + 1) * datetime.timedelta(days=1))
+
+            events = allevents.filter(start__range=(start, end))
+            e.events = events
+            for ev in e.events:
+                ev.time = ev.start.time()
+
+            tmp.append(e)
+            d += 1
+
+        L.append(tmp)
+    return L
+
 def view(request):
     events = get_events()
-    return render(request, 'index.html', {"events":events, "nb_events": range(1, events.count() + 1), 'static_url': STATIC_URL})
+    calendar = initcalendar()
+
+    variables = {}
+    variables['events'] = events
+    variables['nb_events'] = range(1, events.count() + 1)
+    variables['static_url'] = STATIC_URL
+    variables['calendar'] = calendar
+    return render(request, 'index.html', variables)
 
 def logout(request):
     auth.logout(request)
-    events = get_events()
-    return render(request, 'index.html', {'events':events, 'info': "Vous étes déconnecté" })
+    return redirect(reverse('core:index'))
